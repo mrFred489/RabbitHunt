130fc33cd9562876483708a71fe2b19a3211203d
Nickolaj Jepsen <Nickolaj1177@gmail.com>
2015-09-23 13:46:22 +0200

    Removed Files

--- Bush.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Represents a bush. Bushes don't do anything, they're just there.
- * 
- * @author David Matuszek
- * @version October 12, 2001
- */
-public class Bush {
-}
--- Carrot.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Represents a Carrot. Carrots don't do anything, they're just there.
- * 
- * @author Morten D. Bech
- * @version July 29, 2015
- */
-public class Carrot {
-}
